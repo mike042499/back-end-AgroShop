05e4f6d5a4b22225be96838c672c5ca4f9cc1327
Michaell Gonzalez Garcia <miagonzalezg@udistrital.edu.co>
2025-05-30 15:48:08 -0500

    creacion inicial del backend

---
+++ src/main/java/com/example/AgroShop/AgroShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.AgroShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgroShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgroShopApplication.class, args);
+	}
+
+}
